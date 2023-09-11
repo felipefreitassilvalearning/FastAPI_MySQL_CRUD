f33b2f7d091a9eec266d51218487ce6b08c41789
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-09-11 10:43:34 -0300

    Included README

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
+# FastAPI Router
+
+## Como rodar o projeto
+
+### Start the API:
+```
+uvicorn api.main:app
+```
+
+### Start the Database locally
+
+Used local mysql database for this project, you could use docker as well.
+
+Then set the connection string in the config/db.py file.
+
+## Sobre o projeto
+
+Trabalhados alguns conceitos de FastAPI, como:
+- Rotas
+- CRUD
+- Database connection
+
+A temática utilizada foi de estudantes, pois o repositório que serviu de base para esse projeto era de um CRUD de estudantes.
+
 [Original repo: https://github.com/hilalahmad32/fastapi-crud-projects/tree/master/crud-fastapi-mysql](https://github.com/hilalahmad32/fastapi-crud-projects/tree/master/crud-fastapi-mysql)
--- main.py
+++ main.py
@@ -4,18 +4,8 @@ from config.db import con
 from models.index import students
 app=FastAPI()
 
-# best way to make api
-@app.get('/api/students')
-async def index():
-    data=con.execute(students.select()).fetchall()
-    return {
-        "success": True,
-        "data":data
-    }
-
-# insert data
 @app.post('/api/students')
-async def store(student:Student):
+async def store(student: Student):
     data=con.execute(students.insert().values(
         name=student.name,
         email=student.email,
@@ -34,19 +24,32 @@ async def store(student:Student):
             "msg":"Some Problem"
         }
 
-# edit data
+@app.get('/api/students')
+async def index():
+    data=con.execute(students.select()).fetchall()
+    return {
+        "success": True,
+        "data":data
+    }
+
+@app.get('/api/students/{search}')
+async def search(search):
+    data=con.execute(students.select().where(students.c.name.like('%'+search+'%'))).fetchall()
+    return {
+        "success": True,
+        "data":data
+    }
+
 @app.patch('/api/students/{id}')
-async def edit_data(id:int):
+async def edit_data(id: int):
     data=con.execute(students.select().where(students.c.id==id)).fetchall()
     return {
         "success": True,
         "data":data
     }
 
-# update data
-
 @app.put('/api/students/{id}')
-async def update(id:int,student:Student):
+async def update(id: int, student: Student):
     data=con.execute(students.update().values(
         name=student.name,
         email=student.email,
@@ -64,9 +67,8 @@ async def update(id:int,student:Student):
             "msg":"Some Problem"
         }
 
-# delete data
 @app.delete('/api/students/{id}')
-async def delete(id:int):
+async def delete(id: int):
     data=con.execute(students.delete().where(students.c.id==id))
     if data:
         return {
@@ -78,13 +80,3 @@ async def delete(id:int):
             "success": False,
             "msg":"Some Problem"
         }
-
-# search data
-
-@app.get('/api/students/{search}')
-async def search(search):
-    data=con.execute(students.select().where(students.c.name.like('%'+search+'%'))).fetchall()
-    return {
-        "success": True,
-        "data":data
-    }
